7538914c0ccc2e73f987f21cb6005f29bc6cfaa0
margaretmeyers <marg.m.meyers@gmail.com>
2018-08-08 21:25:29 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -10,21 +10,21 @@ coins = {
 
 # least_coins.loop do 
   until cents == 0
-  if cents % 25 == 0 
-   coins[:quarter] += 1
-   cents = cents - 25
-  elsif cents % 10 == 0 
-   coins[:dime] += 1
-   cents = cents - 10
-  elsif cents % 5 == 0
-   coins[:nickel] += 1
-   cents = cents - 5
-  elsif cents % 1 == 0
-   coins[:penny] += 1
-   cents = cents - 1
-  else
-    break
- end 
+    if cents % 25 == 0 
+     coins[:quarter] += 1
+     cents = cents - 25
+    elsif cents % 10 == 0 
+     coins[:dime] += 1
+     cents = cents - 10
+    elsif cents % 5 == 0
+     coins[:nickel] += 1
+     cents = cents - 5
+    elsif cents % 1 == 0
+     coins[:penny] += 1
+     cents = cents - 1
+    else
+      break
+   end 
  end 
  puts coins
 end
